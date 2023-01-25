26f90173bcce9617067fe2774a3660892ba53e62
AndreyMelnikov-dev <1990site@mail.ru>
2023-01-25 18:50:57 +0300

    start api

---
+++ src/Api/auth-api.ts
@@ -0,0 +1,9 @@
+import instance from './instance'
+
+class AuthApi {
+    login() {
+        
+    }
+}
+
+export default new AuthApi()
--- src/Components/UI/Menu/Menu.tsx
+++ src/Components/UI/Menu/Menu.tsx
@@ -10,36 +10,36 @@ const Menu: FC = () => {
             <div className='add-menu'>
                 <NavLink to='' className='add-menu__link'>
                     <img src={logo} alt='icon' />
-                    <span className='link-title f9'>Переводы</span>
+                    <span className='link-title'>Переводы</span>
                 </NavLink>
                 <NavLink to='' className='add-menu__link'>
                     <img src={logo} alt='icon' />
-                    <span className='link-title f9'>Доходы</span>
+                    <span className='link-title'>Доходы</span>
                 </NavLink>
                 <NavLink to='' className='add-menu__link'>
                     <img src={logo} alt='icon' />
-                    <span className='link-title f9'>Расходы</span>
+                    <span className='link-title'>Расходы</span>
                 </NavLink>
             </div>
             <div className='main-menu'>
-                <NavLink to='/' className='main-menu__link'>
+                <NavLink to='/accounts' className='main-menu__link'>
                     <div className='main-menu__icon'>1</div>
                     <div className='f8'>Счета</div>
                 </NavLink>
                 <NavLink to='/' className='main-menu__link'>
                     <div className='main-menu__icon'>2</div>
-                    <div className='f8'>Счета</div>
+                    <div className='f8'>Копилки</div>
                 </NavLink>
                 <button className='main-menu__mainButton'>
                     <img src={plus} alt='main button' />
                 </button>
                 <NavLink to='/' className='main-menu__link'>
                     <div className='main-menu__icon'>3</div>
-                    <div className='f8'>Счета</div>
+                    <div className='f8'>Аналитика</div>
                 </NavLink>
                 <NavLink to='/' className='main-menu__link'>
                     <div className='main-menu__icon'>4</div>
-                    <div className='f8'>Счета</div>
+                    <div className='f8'>Главная</div>
                 </NavLink>
             </div>
         </>
